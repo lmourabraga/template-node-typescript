9a83f95b455593e3ab84c5e6bb2ee56ce7e03772
Luã Braga <ldemourabraga@gmail.com>
2020-07-05 21:21:13 -0300

    Fixing routes
    Including app.use(express.json()); and app.use(routes);

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) =>
-    response.json({ message: 'Hello GoStack' }),
-);
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('Server started on port 3333');
